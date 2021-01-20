b31a203ff756e586bb0bba10770ce92cf38e3edf
Catrina Friday <cat.friday@gmail.com>
2021-01-20 16:58:59 -0500

    added delete and add transaction actions

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,28 @@
-import React, {useState} from 'react'
+import React, {useContext, useState} from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const AddTransaction = () => {
 
     const [text, setText] = useState('')
     const [amount, setAmount] = useState(0)
+    const {addTransaction} = useContext(GlobalContext)
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+           amount: +amount
+           // parseInt(amount) is the same as +amount
+        }
+        addTransaction(newTransaction)
+    }
+
+
     return (
     <>
         <h3>Add new transaction</h3>
-        <form>
+        <form onSubmit={(e) => handleSubmit(e)}>
             <div className="form-control">
                 <label htmlFor="text">Text</label>
                 <input value={text} onChange={(e) => setText(e.target.value)} type="text" placeholder="Enter text..." />
