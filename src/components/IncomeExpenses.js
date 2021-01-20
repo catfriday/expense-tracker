8a54e59556ca814bead34ea58540adee6ec1e5cf
Catrina Friday <cat.friday@gmail.com>
2021-01-20 16:18:18 -0500

    update IncomeExpenses component

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -3,16 +3,22 @@ import { GlobalContext } from '../context/GlobalState'
 
 export const IncomeExpenses = () => {
     const {transactions} = useContext(GlobalContext)
-    
+
+    const amounts = transactions.map(trans => trans.amount)
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
+    const income = amounts.filter(amount => amount > 0).reduce((acc, item) => (acc += item), 0).toFixed(2)
+    const expenses = amounts.filter(amount => amount <= 0).reduce((acc, item) => (acc += item)).toFixed(2)
+    console.log(expenses)
+
     return (
     <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p className="money plus">+$0.00</p>
+                <p className="money plus">+${income}</p>
             </div>
         <div>
           <h4>Expense</h4>
-            <p className="money minus">-$0.00</p>
+            <p className="money minus">-${Math.abs(expenses)}</p>
         </div>
     </div>
     )
